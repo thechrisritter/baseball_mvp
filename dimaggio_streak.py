5587ede1cb0eb3dc7010eef6ac1c9f427a22dcec
Chris Ritter <thechrisritter@gmail.com>
2018-01-12 18:32:16 -0600

    Create dimaggio_streak.py

---
+++ dimaggio_streak.py
@@ -0,0 +1,12 @@
+# dimaggio_streak.py
+#
+# From May 15th to July 16th 1941, Joe Dimaggio batted safely in an unprecidented 56 straight games,
+# a record that still stands to this day. 
+# The feat has been called "the most extraordinary thing that ever happened in American sports."
+# This statistical achievement is believed to be unbreakable.
+# 
+# This is the formula to calculate the odds of such a streak:
+#
+# (1-(1-p)**(ab/n))**n
+# where p = batting percentage, ab = at bats, and n = number of games.
+
